cf0d1c16b31abf18887e4c471952280a5552b126
Gaute <mrboobai@proton.me>
2025-05-04 22:22:47 +0200

    fixed formatting and includes

--- formatJson.c
+++ formatJson.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
 #include "formatJson.h"
 #include "ctype.h"
-#include "stdio.h"
 #include "string.h"
-int i;
-int a;
 
-void ReformatJson(char *json) {
-  for (i = 0; i < strlen(json); i++) {
-    if (json[i] == ':') {
-      a++;
-      printf("%d\n", a);
-    }
-    if (isalpha(json[i])) {
-    }
-  }
-  printf("\n");
+void ReformatJson(char *json) 
+{
+	int a = 0;
+	for (int i = 0; i < strlen(json); i++) {
+		if (json[i] == ':') {
+			a++;
+			printf("%d\n", a);
+		}
+		if (isalpha(json[i])) {
+		}
+	}
+	printf("\n");
 
-  return;
+	return;
 }
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include "formatJson.h"
 #include "getJson.h"
-#include <stdio.h>
 
 int main() 
 {
