00aab59af000e4a26219cd80bacf1c6532a804c8
Gaute <mrboobai@proton.me>
2025-05-04 22:32:37 +0200

    spaces instead of tabs

--- formatJson.c
+++ formatJson.c
@@ -5,16 +5,16 @@
 
 void ReformatJson(char *json) 
 {
-	int a = 0;
-	for (int i = 0; i < strlen(json); i++) {
-		if (json[i] == ':') {
-			a++;
-			printf("%d\n", a);
-		}
-		if (isalpha(json[i])) {
-		}
-	}
-	printf("\n");
+    int a = 0;
+    for (int i = 0; i < strlen(json); i++) {
+        if (json[i] == ':') {
+            a++;
+            printf("%d\n", a);
+        }
+        if (isalpha(json[i])) {
+        }
+    }
+    printf("\n");
 
-	return;
+    return;
 }
