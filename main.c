8ae96e851054f1f4f462bf894b7b7bcc436de7c4
Andolo <90448671+Andolo-64@users.noreply.github.com>
2025-05-04 21:51:42 +0200

    Update main.c formatting

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include "getJson.h"
 #include <stdio.h>
 
-int main() {
-  char jsonLocation[] = "test.json";
-  long length;
+int main() 
+{
+    char jsonLocation[] = "test.json";
+    long length;
 
-  getJsonFileLength(jsonLocation, &length);
-  char jsonText[length + 1];
+    getJsonFileLength(jsonLocation, &length);
+    char jsonText[length + 1];
 
-  getJsonText(jsonLocation, jsonText, &length);
-  printf("%s", jsonText);
-  ReformatJson(jsonText);
+    getJsonText(jsonLocation, jsonText, &length);
+    printf("%s", jsonText);
+    ReformatJson(jsonText);
 
-  return (0);
+    return (0);
 }
