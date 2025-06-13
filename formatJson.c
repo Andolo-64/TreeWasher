bba4ed724ddb85ae50aec7dec77a8a256a921eba
Gaute <mrboobai@proton.me>
2025-06-13 15:36:42 +0200

    blacklist filter

--- formatJson.c
+++ formatJson.c
@@ -1,59 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "formatJson.h"
-#include "ctype.h"
-#include "string.h"
-
-static int size;
-
-/*void spaceAdder(char *jsonText, int depth)
-{
-    // update size
-    size = sizeof(jsonText + depth + 1);
-
-    printf("%s \n before shift: \n", jsonText);
-
-    // shift all elements of jsonText to the right
-    for (int i = strlen(jsonText); i >= 0; i--)
-    {
-        jsonText[i] = jsonText[i - 1];
-    }
-    jsonText[0] = ' ';
-
-    printf("%s \n after shift: \n", jsonText);
-
-
-    // add nummber of spaces == depth jsonText[i] = ' ';
-}*/
+#include <string.h>
+#include <ctype.h>
 
 void reformatJson(char *jsonText)
 {
     char formattedJson[strlen(jsonText) + 1];
+    strcpy(formattedJson, jsonText);
+
+    printf("Original JSON: %s\n", formattedJson);
+
+    char blacklist[] = ".,!?;:'\"()[]{}<>@#$%^&*_-+=~`";
+
+    size_t len = strlen(formattedJson);
     size_t j = 0;
 
-    printf("Original JSON: %s\n", jsonText);
-
-    for (size_t i = 0; i < strlen(jsonText); i++)
-    {
-        if (jsonText[i] == ' ') {
-            if (j > 0 && formattedJson[j - 1] != ' ') {
-                formattedJson[j++] = ' ';
+    for (size_t i = 0; i < len; i++) {
+        int isBlacklisted = 0;
+        for (size_t b = 0; b < strlen(blacklist); b++) {
+            if (formattedJson[i] == blacklist[b]) {
+                isBlacklisted = 1;
+                break;
             }
-        } else if ((jsonText[i] >= 'a' && jsonText[i] <= 'z') ||
-                   (jsonText[i] >= 'A' && jsonText[i] <= 'Z') ||
-                   (jsonText[i] >= '0' && jsonText[i] <= '9')) {
-            formattedJson[j++] = jsonText[i];
+        }
+
+        if (!isBlacklisted) {
+            formattedJson[j++] = formattedJson[i];
         }
     }
 
     formattedJson[j] = '\0';
 
-    printf("Formatted JSON: %s\n", formattedJson);
-
-    // update size
-    // set depth 0
-    // make formattedJson[size];
-    // filter for { == depth++, } == depth--
-    // isalpha
-    // spaceadder and print
+    printf("Modified JSON: %s\n", formattedJson);
 }
+
